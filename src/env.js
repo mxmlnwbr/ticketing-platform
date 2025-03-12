0fee1f951fe9ad936b1ed0aed11e3740569ca6c2
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-03-12 19:24:09 +0100

    retry

--- src/env.js
+++ src/env.js
@@ -7,7 +7,6 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -29,7 +28,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     TICKETING_PLATFORM_DB_URL: process.env.TICKETING_PLATFORM_DB_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
